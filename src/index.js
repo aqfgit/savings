70ee0ef6a0e7d06e6178bfa74e8817a4a3b7836e
Bartosz Daniszewski <bdaniszewski@multic.pl>
2019-06-08 13:52:42 +0200

    Move state to the BudgetController component and integrate it With Budget and Spendsings

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import BudgetController from './components/BudgetController';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BudgetController/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
