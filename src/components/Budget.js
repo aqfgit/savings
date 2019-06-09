6288fdcb343158f532d87645401d9becaa8a5d49
Bartosz Daniszewski <bdaniszewski@multic.pl>
2019-06-09 12:12:38 +0200

    Add basic incomes which add money to the budget every second, even if the user closes the app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Input from "./Input";
 import Button from "./Button";
+import Income from './Income';
 import { getLocalStorageItem } from '../utils/localStorage';
 
 class Budget extends React.Component {
+
   render() {
+   
     return (
       <>
         <h2>My budget</h2>
@@ -15,6 +18,11 @@ class Budget extends React.Component {
         />
         <Button onClick={this.props.addToBudget} name="Add to budget" />
         <p>Balance: {getLocalStorageItem('balance') || 0}$</p>
+
+        <h3>Incomes</h3>
+        <ul>
+            
+        </ul>
       </>
     );
   }
